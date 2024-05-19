fb66cb020ea8ca8e977b507c881bfce5589b60a2
Bernardino Ochoa <bernardo.ochoa@concentrix.com>
2024-05-20 04:03:51 +0800

    Co-authored-by: abbeybrags <abbeybrags@users.noreply.github.com>

--- inventory_bacakend/index.js
+++ inventory_bacakend/index.js
@@ -20,27 +20,46 @@ db.connect((err) => {
   console.log("Connected to database");
 });
 
-//create table script
-db.query(
-  "CREATE TABLE IF NOT EXISTS products (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), price INT, quantity INT, category VARCHAR(255), description VARCHAR(256), brand VARCHAR(255))",
-  (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Table created");
-  },
-);
+// Create suppliers table
+const createSuppliersTableQuery = `
+  CREATE TABLE IF NOT EXISTS suppliers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    address VARCHAR(255),
+    mobile VARCHAR(255)
+  )
+`;
 
-//create supplier table
-db.query(
-  "CREATE TABLE IF NOT EXISTS suppliers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255), mobile VARCHAR(255), product_id INT, FOREIGN KEY (product_id) REFERENCES products(id))",
-  (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Table created");
-  },
-);
+db.query(createSuppliersTableQuery, (err, result) => {
+  if (err) {
+    console.error('Error creating suppliers table: ', err);
+    return;
+  }
+  console.log("Suppliers table created");
+});
+
+// Create products table
+const createProductsTableQuery = `
+  CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    price INT,
+    quantity INT,
+    category VARCHAR(255),
+    description VARCHAR(256),
+    brand VARCHAR(255),
+    supplier_id INT,
+    FOREIGN KEY (supplier_id) REFERENCES suppliers(id)
+  )
+`;
+
+db.query(createProductsTableQuery, (err, result) => {
+  if (err) {
+    console.error('Error creating products table: ', err);
+    return;
+  }
+  console.log("Products table created");
+});
 
 
 
@@ -54,20 +73,22 @@ app.get("/products", (req, res) => {
     if (err) {
       throw err;
     }
+    console.log(result);
     res.send(result);
   });
 });
 
 //create product
 app.post("/product", (req, res) => {
-  const { name, price, quantity, category, description, brand } = req.body;
+  const { name, price, quantity, category, description, brand, supplier_id } = req.body;
   db.query(
-    "INSERT INTO products (name, price, quantity, category, description, brand) VALUES (?,?,?,?,?,?)",
-    [name, price, quantity, category, description, brand],
+    "INSERT INTO products (name, price, quantity, category, description, brand, supplier_id) VALUES (?,?,?,?,?,?,?)",
+    [name, price, quantity, category, description, brand, supplier_id],
     (err, result) => {
       if (err) {
         throw err;
       }
+      console.log(result);
       res.send("Product added");
     },
   );
@@ -84,6 +105,7 @@ app.put("/product/:id", (req, res) => {
       if (err) {
         throw err;
       }
+      console.log(result);
       res.send("Product updated");
     },
   );
@@ -96,9 +118,92 @@ app.delete("/product/:id", (req, res) => {
     if (err) {
       throw err;
     }
+    console.log(result);
     res.send("Product deleted");
   });
 });
+
+
+//get product by supplier id 
+
+app.get("/supplier/:supplierId/products", (req, res) => {
+  const supplierId = req.params.supplierId;
+  db.query("SELECT * FROM products WHERE supplier_id = ?", supplierId, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
+    res.send(result);
+  }
+  );
+});
+
+
+//get suppliers
+app.get("/suppliers", (req, res) => {
+  db.query("SELECT * FROM suppliers", (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
+});
+
+//create supplier
+app.post("/supplier", (req, res) => {
+  const { name, address, mobile } = req.body;
+  db.query(
+    "INSERT INTO suppliers (name, address, mobile) VALUES (?,?,?)",
+    [name, address, mobile],
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send("Supplier added");
+    },
+  );
+});
+
+//update supplier
+
+app.put("/supplier/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, address, mobile } = req.body;
+  db.query(
+    "UPDATE suppliers SET name = ?, address = ?, mobile = ? WHERE id = ?",
+    [name, address, mobile, id],
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send("Supplier updated");
+    },
+  );
+});
+
+//delete supplier
+
+app.delete("/supplier/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM suppliers WHERE id = ?", id, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send("Supplier deleted");
+  });
+});
+
+
+//get supplier list [{supplier name: supplier id}]
+app.get("/supplierlist", (req, res) => {
+  db.query("SELECT id, name FROM suppliers", (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
